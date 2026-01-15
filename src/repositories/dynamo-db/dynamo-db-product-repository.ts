66f5063041b839af00dad5f2a99e19bc635d8969
Vitor Mendonça Braga <vmbbraga5@gmail.com>
2026-01-15 08:26:09 -0300

    feat(repository): add findProductsByCategory function implementation on dynamo db repository

--- src/repositories/dynamo-db/dynamo-db-product-repository.ts
+++ src/repositories/dynamo-db/dynamo-db-product-repository.ts
@@ -6,13 +6,13 @@ import { ProductRepository } from "../product-repository";
 
 export class DynamoDbProductRepository implements ProductRepository {
     private readonly client: DynamoDBDocumentClient;
-    private readonly tableName = "ProdutosBestsellers"; 
+    private readonly tableName = "ProdutosBestsellers";
 
     constructor() {
         const baseClient = new DynamoDBClient({});
         this.client = DynamoDBDocumentClient.from(baseClient);
     }
-    
+
     async findTop3ByCategory(category: Categories): Promise<Product[]> {
         const command = new QueryCommand({
             TableName: this.tableName,
@@ -21,11 +21,29 @@ export class DynamoDbProductRepository implements ProductRepository {
             ExpressionAttributeValues: {
                 ":cat": category,
             },
-            Limit: 3, 
+            Limit: 3,
             ScanIndexForward: true,
         });
 
         const response = await this.client.send(command);
         return response.Items as Product[]
+    }
+
+    async findProductsByCategory(category: Categories, page: number): Promise<Product[]> {
+        const NUMBER_ITEMS_PER_PAGE = 10;
+        const limit = page * NUMBER_ITEMS_PER_PAGE;
+        const command = new QueryCommand({
+            TableName: this.tableName,
+            IndexName: "CategoryIndex",
+            KeyConditionExpression: "category = :cat",
+            ExpressionAttributeValues: {
+                ":cat": category,
+            },
+            Limit: limit,
+            ScanIndexForward: true,
+        });
+
+        const response = await this.client.send(command);
+        return response.Items?.splice((page - 1) * NUMBER_ITEMS_PER_PAGE, page * NUMBER_ITEMS_PER_PAGE) as Product[]
     }
 }
