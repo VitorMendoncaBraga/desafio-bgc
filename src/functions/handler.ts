6940a6a9a5b6db11b989e1dec4f7f61cef12a35c
Vitor Mendonça Braga <vmbbraga5@gmail.com>
2026-01-15 09:15:37 -0300

    feat(handler): add getAllBestSellersByCategory lambda function

--- src/functions/handler.ts
+++ src/functions/handler.ts
@@ -2,10 +2,29 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { DynamoDbProductRepository } from 'src/repositories/dynamo-db/dynamo-db-product-repository';
 import { ProductRepository } from 'src/repositories/product-repository';
 import { ValidationError } from 'src/use-cases/errors/validation-error';
+import { GetAllBestSellersByCategory } from 'src/use-cases/get-all-bestsellers-by-category';
 import { GetTop3BestSellersByCategory } from 'src/use-cases/get-top-3-bestsellers-by-category';
 import z, { ZodError } from 'zod'
 
-const QuerySchema = z.object({
+const GetTop3BestSellersByCategoryQuerySchema = z.object({
+    category: z.enum([
+        'books',
+        'fashion',
+        'kitchen',
+        'home',
+        'appliances',
+        'electronics',
+        'sports',
+        'videogames',
+        'furniture',
+        'pet-products'
+    ])
+})
+
+const GetAllBestSellersByCategoryQuerySchema = z.object({
+    page: z.coerce.number().default(1)
+})
+const GetAllBestSellersByCategoryParamsSchema = z.object({
     category: z.enum([
         'books',
         'fashion',
@@ -22,10 +41,11 @@ const QuerySchema = z.object({
 
 const productRepository: ProductRepository = new DynamoDbProductRepository()
 const getTop3BestSellersByCategoryUseCase = new GetTop3BestSellersByCategory(productRepository)
+const getAllBestSellersByCategoryUseCase = new GetAllBestSellersByCategory(productRepository)
 
 exports.getTop3BestSellersByCategory = async function (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
     try {
-        const { category } = QuerySchema.parse(event.queryStringParameters)
+        const { category } = GetTop3BestSellersByCategoryQuerySchema.parse(event.queryStringParameters)
         const products = await getTop3BestSellersByCategoryUseCase.execute({ category })
         return {
             statusCode: 200,
@@ -44,11 +64,46 @@ exports.getTop3BestSellersByCategory = async function (event: APIGatewayProxyEve
         }
 
         return {
-                statusCode: 500,
+            statusCode: 500,
+            body: JSON.stringify({
+                error: "Internal server error",
+                message: error
+            })
+        }
+    }
+}
+
+exports.getAllBestSellersByCategory = async function (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    try {
+        const { page } = GetAllBestSellersByCategoryQuerySchema.parse(event.queryStringParameters || {})
+        const { category } = GetAllBestSellersByCategoryParamsSchema.parse(event.pathParameters)
+
+        const { products } = await getAllBestSellersByCategoryUseCase.execute({ page, query: category })
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                products
+            })
+        }
+    } catch (error) {
+        if (error instanceof ZodError) {
+            return {
+                statusCode: 400,
                 body: JSON.stringify({
-                    error: "Internal server error",
+                    error: "Error of parameters validation",
                     message: error
                 })
             }
+        }
+
+        console.log(error)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: "Internal server error",
+                message: error
+            })
+        }
     }
 }
