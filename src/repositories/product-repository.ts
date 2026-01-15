572c204f4f563d5343ae25cff80ba5a422b9ce2c
Vitor Mendonça Braga <vmbbraga5@gmail.com>
2026-01-15 13:02:29 -0300

    feat(repository): add findManyByTitle method in interface

--- src/repositories/product-repository.ts
+++ src/repositories/product-repository.ts
@@ -3,4 +3,5 @@ import { Categories, Product } from "src/entities/product";
 export interface ProductRepository {
     findTop3ByCategory(category: Categories) : Promise<Product[]>
     findProductsByCategory(category: Categories, page: number): Promise<Product[]>
+    findManyByTitle(query: string, page: number) : Promise<Product[]>
 }
